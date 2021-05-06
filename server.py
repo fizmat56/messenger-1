4855df368eecada8fad3127888ae83439569b363
Vladimir Ivashentsov <Vladimir.Ivashentsov@vrbank.ru>
2021-05-07 00:13:57 +0300

    task1

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
-
+myChatName = 'FM56chat 1.0'
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
-
 @app.route("/status")
 def status():
-    return "Hello"
-
+    return {
+        'status': True,
+        'name': myChatName,
+        'time': time.asctime()
+    }
 
 app.run()
